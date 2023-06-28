5c13b2d93e0e03e5b1c29caa213f95548e8e5267
24Lights <134679427+24Lights@users.noreply.github.com>
2023-06-28 17:46:10 +0530

    Create head.h

---
+++ head.h
@@ -0,0 +1,18 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+// LIBS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <sstream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <cstdlib>
+#include <cstdio>
+#include <signal.h>
+#include <readline/readline.h>
+#include <readline/history.h>
